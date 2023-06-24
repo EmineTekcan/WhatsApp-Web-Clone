6f24ab8339cb0dbbd0f8f7c9ac2e8e722358b050
Emine Tekcan <eminetek68can@gmail.com>
2023-06-24 11:08:53 +0300

    Created Right Side in no message

--- src/components/ChatCard/ChatCard.jsx
+++ src/components/ChatCard/ChatCard.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 
 const ChatCard = () => {
-  return (
-    <div>ChatCard</div>
-  )
+    return (
+        <div className='flex items-center justify-center py-2 group cursor-pointer mx-2'>
+
+            <div className='w-[20%]'>
+                <img
+                    className='h-14 w-14 rounded-full'
+                    src='https://cdn.pixabay.com/photo/2023/06/14/06/22/cat-8062388_1280.jpg'
+                    alt='Other People Image' />
+            </div>
+
+            <div className='pl-2 w-[90%]'>
+                <div className='flex justify-between items-center'>
+                    <p className='text-lg'>username</p>
+                    <p className='text-sm' >timestamp</p>
+                </div>
+
+                <div className='flex justify-between items-center'>
+                    <p>message...</p>
+                    <div className='flex space-x-2 items-center'>
+                        <p className='text-xs py-1 px-2 text-white bg-green-500 rounded-full'>5</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
 }
 
 export default ChatCard
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { TbCircleDashed } from 'react-icons/tb'
 import { BiCommentDetail } from 'react-icons/bi'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { BsFilter } from 'react-icons/bs'
+import ChatCard from './ChatCard/ChatCard'
+import bg from '../assets/bg.PNG'
 
 const HomePage = () => {
+
+  const [queries, setQueries] = useState("");
+  const [currentChat, setCurrentChat] = useState(null);
+
+  const handleSearch = () => {
+
+  }
+
+  const handleClickChatCard = () => {
+    setCurrentChat(true);
+  }
+
   return (
     <div className='relative'>
       <div className=' py-14 bg-[#00a884]  w-full '>
       </div>
 
-      <div className='flex bg-[#f0f2f5] h-[90vh] absolute top-6 left-6 w-full'>
+      <div className='flex bg-[#f0f2f5] h-[90vh] absolute top-6 mx-6 w-full'>
 
         {/* Left section */}
         <div className='left-0 w-[30%] h-full bg-[#e8e9ec]'>
@@ -34,9 +48,14 @@ const HomePage = () => {
               <div className='flex flex-row text-xs lg:text-base bg-slate-200 rounded-md w-[93%] pl-2 py-2' >
                 <AiOutlineSearch size={20} />
                 <input
+                  value={queries}
                   className='bg-slate-200  border-none outline-none pl-2'
                   type='text'
                   placeholder='Search or start new chat'
+                  onChange={(e) => {
+                    setQueries(e.target.value);
+                    handleSearch(e.target.value);
+                  }}
                 />
               </div>
               <div className='ml-2'>
@@ -46,11 +65,29 @@ const HomePage = () => {
 
           </div>
 
+          {/* All Users Messages section */}
+          <div className='bg-white overflow-y-scroll h-[76.8v]'>
+            {queries && [1, 1, 1, 1].map((item) =>
+              <div onClick={() => handleClickChatCard}>
+                <hr />
+                <ChatCard />
+              </div>
+            )}
+          </div>
+
+
+
         </div>
 
         {/* Right section */}
-        <div className='right-0'>
-
+        <div className='right-0 flex items-center justify-center'>
+          <div className='w-[70%] flex flex-col items-center justify-center h-full'>
+            <div className='w-full  self-center text-center'>
+              <img src={bg} alt='bg-image' />
+              <h1 className='text-4xl text-gray-600'>WhatsApp Web</h1>
+              <p className='my-9'>send and receive message without keeping your phone online. Use WhatsApp on Up to 4 Linked devices and 1 phone at the same time</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
